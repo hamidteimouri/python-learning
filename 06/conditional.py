71a19c6f10c4bfef53a702b43beb0161d82cca64
hamidteimouri <hamidteimouri@outlook.com>
2018-11-19 21:57:59 +0330

    if conditional

--- 06/conditional.py
+++ 06/conditional.py
@@ -1,6 +1,11 @@
 a, b = 3, 2
 
+# type a
 if a < b:
     print(" a ({}) less than b ({})".format(a, b))
 else:
     print(" a ({}) greater than b ({})".format(a, b))
+
+
+# type b
+print("x" if a > b else "y")
